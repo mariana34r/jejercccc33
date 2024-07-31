a1c5ccc4064dfada59d2b0425940b2920ccc3a9c
mariana34r <cesaragustoruedarangel@gmail.com>
2024-07-31 07:27:21 -0500

    feat: :sparkles: finaly repository

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import registrar_users
 import cargar
+import edit_users
+import mostrar_user
 
 
 while True:
-    print("Binevenido :)\n que quiere hacer\n1. registrarse\n2.iniciar seccion\n3.mirar usuarios\n4.eliminar usuarios\n5.salir")
+    print("Binevenido :)\n que quiere hacer\n1. registrarse\n2.iniciar seccion\n3.mirar usuarios\n4.eliminar usuarios\n5.Editar Usuario\n6.Editar Usuario")
     print("Ingrese una opcion: ")
     try:
         opc=int(input(""))
@@ -15,9 +17,13 @@ while True:
             cargar.cargar_user()
         elif opc== 3:
             print("Bienvenido a mirar usuarios")
+            mostrar_user.mostrar()
         elif opc== 4:
             print("Bienvenido a eliminar ususarios")
         elif opc== 5:
+            print("Bienvenido a editar ususarios")
+            edit_users.edit_users()
+        elif opc== 6:
             print("Saliendoooooo")
             break
         else:
