a248432047ba9ab027ce891fd041bc5f6203c0eb
mariana34r <cesaragustoruedarangel@gmail.com>
2024-07-31 07:12:54 -0500

    menuuuuuu

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-print("Binevenido :)\n que quiere hacer\n1. registrarse\n2.iniciar seccion\n3.mirar usuarios\n4.eliminar usuarios\n5.salir")
+import registrar_users
+import cargar
 
 
 while True:
+    print("Binevenido :)\n que quiere hacer\n1. registrarse\n2.iniciar seccion\n3.mirar usuarios\n4.eliminar usuarios\n5.salir")
     print("Ingrese una opcion: ")
     try:
         opc=int(input(""))
         if opc == 1 :
             print("Binevenido a resgistrar Usuario")
+            registrar_users.regis()
         elif opc== 2:
             print("Bienvenido a iniciar seccion")
+            cargar.cargar_user()
         elif opc== 3:
             print("Bienvenido a mirar usuarios")
         elif opc== 4:
--- registrar_users.py
+++ registrar_users.py
@@ -1,15 +1,29 @@
 import cargar as gg
 def regis ():
     data = gg.cargar_user()
-    docu = int(input("DIgite su documento :"))
+    while True:
+        try:
+            docu = int(input("DIgite su documento :"))
+            break
+        except Exception:
+            print("Ingrese un numero validoo")
+
     if str(docu) in data["users"]:
-        print("Ya hay un usuario registrado con ese documento")
+            print("Ya hay un usuario registrado con ese documento")
+
+
     else:
         datos = {}
         datos["Nombre"] = str(input("Digite su nombre: "))
         datos["Apellido"] = str(input("Digite su apellido: "))
-        datos["Edad"] = int(input("Ingrese su edad: "))
-        data["users"][docu]=datos
-        gg.guarda_user(data)
+        while True:
+            try:
+                datos["Edad"] = int(input("Ingrese su edad: "))
+                break
+            except Exception:
+                print("Ingrese una edad valida")
+
+    data["users"][docu]=datos
+    gg.guarda_user(data)
 
 
